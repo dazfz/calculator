e76be043bbc69f73c771814d6c18408cfc0bff86
dazfz <105754662+dazfz@users.noreply.github.com>
2023-05-01 18:12:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+let input = '', x, y, op = '', hasOperator = false;
+
+const operate = (x, y, op) => {
+    switch (op) {
+        case '+': return add(x, y);
+        case '−': return subtract(x, y);
+        case '×': return multiply(x, y);
+        case '÷': return divide(x, y);
+    }
+}
+
+const numButtons = document.querySelectorAll('.num');
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (hasOperator) return;
+        input += button.textContent;
+        display.textContent = input;
+    });
+});
+
+const opButtons = document.querySelectorAll('.op');
+opButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        hasOperator = false;
+        // guardar el primer input
+        if (x == null) x = parseFloat(input);
+        // guardar los demas input
+        else if (x != null && y == null && input != '') {
+            y = parseFloat(input);
+            x = operate(x, y, op); // hacer la operacion (x,y) y guardarlo en x
+            // reiniciar el input y deja ingresar numeros denuevo
+            y = null;
+        }
+        display.textContent = x, input = '';
+        // cambio la operacion al final, para que haga la operacion con el segundo numero primero
+        op = button.textContent;
+        if (op == '=') y = null, hasOperator = true;
+    });
+});
+
+const ac = () => {
+    x = y = null; input = op = ''; hasOperator = false;
+    display.textContent = input;
+}
+const neg = () => {
+    if (hasOperator) x *= -1, display.textContent = x;
+    else {
+        input = input.startsWith('-') ? input.slice(1) : `-${input}`;
+        display.textContent = input;
+    }
+}
+
+const del = () => {
+    if (!hasOperator) {
+        input = input.slice(0, -1);
+        display.textContent = input;
+    }
+};
+
+const modButtons = document.querySelectorAll('.mod');
+modButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        switch (button.textContent) {
+            case 'AC': {
+                ac();
+                break;
+            }
+            case '+/−': {
+                neg();
+                break;
+            }
+            case 'DEL': {
+                del();
+                break;
+            }
+        }
+    });
+});
+
+document.addEventListener('keydown', (event) => {
+    // Si la tecla presionada es un número, agregamos el dígito al input
+    if (/^[0-9]$/.test(event.key)) {
+        if (hasOperator) return;
+        input += event.key;
+        display.textContent = input;
+    }
+    // Si la tecla presionada es un operador, ejecutamos la operación correspondiente
+    else if (/^[\+\-\*\/]$/.test(event.key) || event.key === 'Enter') {
+        hasOperator = false;
+        if (x == null) x = parseFloat(input);
+        else if (x != null && y == null && input != '') {
+            y = parseFloat(input);
+            x = operate(x, y, op);
+            y = null;
+        }
+        display.textContent = x, input = '';
+        op = event.key;
+        if (op == 'Enter') y = null, hasOperator = true;
+    }
+    // Si la tecla presionada es Delete, borramos el último dígito
+    else if (event.key === 'Backspace') {
+        del();
+    }
+    // Si la tecla presionada es Escape, borramos todo
+    else if (event.key === 'Escape') {
+        ac();
+    }
+    // Si la tecla presionada es s, cambiamos de signo
+    else if (event.key === 's') {
+        neg();
+    }
+});
+
